8256ef0421af15d76f029c95009527ea759d406d
Ashto <contact@gustavoguarino.com>
2018-11-23 11:40:58 -0500

    Tweaked spacing between elements
    ** Changed **
    
    * Made sizing of elements relative to #app's font size
      for simple resizing
    * Class renaming
      * .change-minute-button => .minute-button
      * .main-display => .countdown-display
      * .toggle-timer-button => .toggle-countdown-button
    * Linted vue files, removed semicolons and spaced accordingly

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,120 @@
 <template>
-  <div id="app">
-    <div class="main-display">{{ timeDisplay }}</div>
-    <section v-if="!timerActive" class="timer-container">
-      <Timer v-for="(timer, index) in timers"
-             :key="index"
-             :timer-type="timer.type"
-             v-model:seconds="timer.max"></Timer>
-    </section>
-    <button class="toggle-timer-button" @click="toggleTimer">{{ timerActive ? 'stop' : 'start' }}</button>
-  </div>
+<div id="app">
+  <div class="countdown-display">{{ timeDisplay }}</div>
+  <section v-if="!timerActive" class="timer-container">
+    <Timer v-for="(timer, index) in timers"
+           :key="index"
+           :timer-type="timer.type"
+           v-model="timer.max"></Timer>
+  </section>
+  <button class="toggle-countdown-button" @click="toggleTimer">{{ timerActive ? 'stop' : 'start' }}</button>
+</div>
 </template>
 
 <style lang="sass">
-  button
-    background: none
-    border: none
-    padding: 0
-    &:hover
-      border-style: solid
-      line-height: 1
-  #app
-    font-size: 3.5vmin
-    text-align: center
-    width: 80%
-    position: absolute
-    left: 50%
-    top: 50%
-    transform: translate(-50%, -50%)
-    font-family: 'Droid Sans Mono', sans-serif
-    text-transform: lowercase
-  
-  .main-display
-    margin-bottom: 16px
-    font-size: 4em
+button
+  background: none
+  border: none
+  padding: 0
+  line-height: 1em
+  &:hover
+    border-style: solid
+#app
+  font-size: 24px
+  text-align: center
+  position: absolute
+  left: 50%
+  top: 50%
+  width: 80%
+  max-width: 960px
+  transform: translate(-50%, -50%)
+  font-family: 'Droid Sans Mono', sans-serif
+  text-transform: lowercase
 
-  .timer-container
-    display: flex
-    flex-wrap: wrap
-    justify-content: center
-    
-  .toggle-timer-button
-    font-size: 1.5em
-    margin-top: 16px
-    height: 50px
-    width: 100px
+.countdown-display
+  display: flex
+  justify-content: center
+  align-items: flex-end
+  margin-bottom: 0.5em
+  font-size: 2em
+
+.timer-container
+  display: flex
+  flex-wrap: wrap
+  justify-content: center
+
+.toggle-countdown-button
+  font-size: 1.25em
+  height: 2em
+  margin-top: 0.25em
+  padding: 0 0.7em
 </style>
 
 <script>
-  import Timer from './components/Timer.vue'
+import Timer from './components/Timer.vue'
 
-  export default {
-    name: 'app',
-    components: {
-      Timer
-    },
-    data() {
-      return {
-        timers: [
-          {
-            type: 'Work',
-            max: 1500,
-            startMessage: 'Back to the grind!'
-          },
-          {
-            type: 'Rest',
-            max: 300,
-            startMessage: 'Break time!'
-          }
-        ],
-        timerActive: false,
-        displayTimerMessage: false,
-        current: 0,
-        seconds: 3000
+export default {
+  name: 'app',
+  components: {
+    Timer
+  },
+  data () {
+    return {
+      timers: [
+        {
+          type: 'Work',
+          max: 1500,
+          startMessage: 'Back to the grind!'
+        },
+        {
+          type: 'Rest',
+          max: 300,
+          startMessage: 'Break time!'
+        }
+      ],
+      timerActive: false,
+      displayTimerMessage: false,
+      timeout: null,
+      current: 0,
+      seconds: 3000
+    }
+  },
+  methods: {
+    toggleTimer () {
+      this.timerActive = !this.timerActive
+      if (this.timerActive) {
+        this.startCountdown()
+      } else {
+        this.displayTimerMessage = false
+        clearTimeout(this.interval)
       }
     },
-    methods: {
-      toggleTimer() {
-        this.timerActive = !this.timerActive;
-        if (this.timerActive) {
-          this.startCountdown();
+    startCountdown () {
+      this.displayTimerMessage = false
+      this.seconds = this.timers[this.current].max + 1
+      this.tick()
+    },
+    tick () {
+      if (this.timerActive) {
+        this.seconds--
+        if (this.seconds <= 0) {
+          this.current = this.current < this.timers.length - 1 ? this.current + 1 : 0
+          this.displayTimerMessage = true
+          this.interval = setTimeout(this.startCountdown, 5000)
         } else {
-          this.displayTimerMessage = false;
+          this.interval = setTimeout(this.tick, 1000)
         }
-      },
-      startCountdown() {
-        this.displayTimerMessage = false;
-        this.seconds = this.timers[this.current].max + 1;
-        this.tick();
-      },
-      tick() {
-        if (this.timerActive) {
-          this.seconds--;
-          if (this.seconds <= 0) {
-            this.current = this.current < this.timers.length - 1 ? this.current + 1 : 0;
-            this.displayTimerMessage = true;
-            setTimeout(this.startCountdown, 5000);
-          } else {
-            setTimeout(this.tick, 1000);
-          }
-        }
-      }
-    },
-    computed: {
-      timeDisplay() {
-        if (this.displayTimerMessage) {
-          return this.timers[this.current].startMessage;
-        }
-        let t = this.timerActive ? this.seconds : this.timers[this.current].max;          
-        return Math.floor(t / 60) + ':' + new String(t % 60).padStart(2, '0');
       }
     }
+  },
+  computed: {
+    timeDisplay () {
+      if (this.displayTimerMessage) {
+        return this.timers[this.current].startMessage
+      }
+      let t = this.timerActive ? this.seconds : this.timers[this.current].max
+      return Math.floor(t / 60) + ':' + `${t % 60}`.padStart(2, '0')
+    }
   }
+}
 </script>
--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,65 +1,66 @@
 <template>
-  <div class="timer">
-    <span class="timer-type">{{ timerType }} time</span>
-    <div class="timer-controls">
-      <button class="change-minute-button" :disabled="seconds >= max" :class="{disabled: seconds >= max}" @click="changeMinute(1)">+</button>
-      <span class="minute-display">{{ timerDisplay }}</span>
-      <button class="change-minute-button" :disabled="seconds <= min" :class="{disabled: seconds <= min}" @click="changeMinute(-1)">-</button>
-    </div>
+<div class="timer">
+  <span class="timer-type">{{ timerType }} time</span>
+  <div class="timer-controls">
+    <button class="minute-button" :disabled="seconds >= max" :class="{disabled: seconds >= max}" @click="changeMinute(1)">+</button>
+    <span class="minute-display">{{ timerDisplay }}</span>
+    <button class="minute-button" :disabled="seconds <= min" :class="{disabled: seconds <= min}" @click="changeMinute(-1)">-</button>
   </div>
+</div>
 </template>
 
 <style lang="sass">
-  .timer
-    margin: 10px
-  .minute-display
-    margin: 10px
-  .timer-controls
-    display: flex
-    justify-content: center
-    align-items: center
-  .change-minute-button
-    font-size: 1.5em
-    height: 50px
-    width: 50px
+.timer, .minute-display
+  flex: 0 1 45%
+  margin: 0.5em
+.minute-display
+  min-width: 5em
+.timer-controls
+  display: flex
+  justify-content: center
+  align-items: center
+.minute-button
+  font-size: 1em
+  height: 1.5em
+  width: 1.5em
 </style>
 
 <script>
-  export default {
-    model: {
-      prop: 'seconds',
-      event: 'change'
+export default {
+  model: {
+    prop: 'seconds',
+    event: 'change'
+  },
+  name: 'Timer',
+  props: {
+    timerType: {
+      type: String
     },
-    name: 'Timer',
-    props: {
-      timerType: {
-        type: String
-      },
-      seconds: {
-        type: Number,
-        default: 600
+    seconds: {
+      type: Number,
+      default: 600
+    }
+  },
+  data () {
+    return {
+      max: 3600,
+      min: 60
+    }
+  },
+  methods: {
+    changeMinute (amount) {
+      amount *= 60
+      if (this.seconds + amount < this.min || this.seconds + amount > this.max) {
+        amount = 0
       }
-    },
-    data() {
-      return {
-        max: 3600,
-        min: 60
-      }
-    },
-    methods: {
-      changeMinute(amount) {
-        amount *= 60;
-        if (this.seconds + amount < this.min || this.seconds + amount > this.max) {
-          amount = 0;
-        }
-        this.$emit('change', this.seconds + amount);
-      }
-    },
-    computed: {
-      timerDisplay() {
-        let m = Math.floor(this.seconds / 60);
-        return `${m} min${m > 1 ? 's' : ''}`;
-      }
+      this.$emit('change', this.seconds + amount)
+    }
+  },
+  computed: {
+    timerDisplay () {
+      let m = Math.floor(this.seconds / 60)
+      return `${m} min${m > 1 ? 's' : ''}`
     }
   }
+}
 </script>
